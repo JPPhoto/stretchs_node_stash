69290e067c717305c5ece723988d11af5137fd42
Stretch <ctwildgoose@gmail.com>
2024-08-29 21:35:37 +0100

    Added extension selection to LoadAllTextFilesInFolder

--- string_tools.py
+++ string_tools.py
@@ -58,16 +58,17 @@ class LoadAllTextFilesInFolderOutput(BaseInvocationOutput):
     result: list[str] = OutputField(title="Results")
 
 
-@invocation("load_all_text_files_in_folder_output", title="Load All Text Files In Folder", tags=["string"], category="string", version="1.0.0", use_cache=False)
+@invocation("load_all_text_files_in_folder_output", title="Load All Text Files In Folder", tags=["string"], category="string", version="1.0.1", use_cache=False)
 class LoadAllTextFilesInFolderInvocation(BaseInvocation):
     """Loads all text files in a folder and returns them as a Collection of strings"""
 
+    extension_to_match: str = InputField(title="File Extension", description="Only files with the given extension will be loaded. For example: json")
     folder_path: str = InputField(title="Folder Path", description="The path to load files from.")
 
     def invoke(self, context: InvocationContext) -> LoadAllTextFilesInFolderOutput:
         files_content = []
         for filename in os.listdir(self.folder_path):
-            if filename.endswith("json"):
+            if filename.endswith(self.extension_to_match):
                 file_path = os.path.join(self.folder_path, filename)
                 with open(file_path, 'r') as file:
                     files_content.append(file.read())
